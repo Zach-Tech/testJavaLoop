eb9ffa998fc6971205b4a760ca52afb89ce99499
zachc <childers.zd@gmail.com>
2021-01-13 13:50:51 -0500

    Now working on Java assignment. Need to separate these into void, and add overtime calculations. Will work on that soon.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,22 @@
 package com.company;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     private String reRun;
     public static void main(String[] args) {
 	// write your code here
        String yes = "";
+       int hours = 0;
+       double rate, tot = 0;
        Scanner input = new Scanner(System.in);
-       do{System.out.println("Testing, 1...2...3...\n");
-       System.out.println("Run again?\nYes//No: ");
+       do{System.out.println("How many hours did you work: ");
+       hours = input.nextInt();
+       System.out.println("Whats your rate: ");
+       rate = input.nextInt();
+       tot = (hours*rate);
+       System.out.printf("Hours x Rate: %s", tot);
+       System.out.println("\nRun again?\nYes//No: ");
        yes = input.next();
            }while(yes.equalsIgnoreCase("yes"));}
 
